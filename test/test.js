625c1241fcdd4235ed4a0555db4a4fc0bc228f67
marcuscosinus <bhconti@gmail.com>
2018-08-24 16:41:55 +0200

    simple test file

--- test/test.js
+++ test/test.js
@@ -2,7 +2,15 @@
 
 const EAkta = require('../lib/EAkta')
 
-let es3File = './files/ill.igazolas.temp.es3'
+let files = [
+  'ill.igazolas',
+  '161213_RentYourDeveloper Kft._valtozasbejegyzo vegzes',
+  'RentYourDeveloper Kft._beadas',
+  'terti',
+  'TULLAP06052018080814323000-60117337389-117337414-3076765450697434536457534'
+]
+
+let es3File = `./files/${files[0]}.es3`
 
 EAkta
   .createEAktaFromFile(es3File)
